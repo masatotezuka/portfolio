defd98000b8a6516da1ace9af294c41e8b2974d7
masatotezuka <tez.0731.mst@gmail.com>
2023-11-11 00:21:49 +0900

    add skill list

--- constans/index.ts
+++ constans/index.ts
@@ -5,3 +5,56 @@ export const NAV_ITEMS = [
   { to: 'links', label: 'LINKS' },
   { to: 'contact', label: 'CONTACT' }
 ]
+
+export const SKILL_LIST = [
+  {
+    name: 'TypeScript',
+    image: '/skills/typescript-icon.svg'
+  },
+  {
+    name: 'JavaScript',
+    image: '/skills/javascript.svg'
+  },
+
+  {
+    name: 'Node.js',
+    image: '/skills/nodejs.png'
+  },
+  {
+    name: 'Express',
+    image: '/skills/express.svg'
+  },
+  {
+    name: 'NestJS',
+    image: '/skills/nestjs.svg'
+  },
+  {
+    name: 'Next.js',
+    image: '/skills/nextjs-icon.svg'
+  },
+  {
+    name: 'React',
+    image: '/skills/react.svg'
+  },
+  {
+    name: 'Prisma',
+    image: '/skills/prisma.svg'
+  },
+  {
+    name: 'TypeORM',
+    image: '/skills/typeorm.svg'
+  },
+  {
+    name: 'Sequelize',
+    image: '/skills/sequelize.svg'
+  },
+
+  {
+    name: 'MySQL',
+    image: '/skills/mysql.svg'
+  },
+  {
+    name: 'PostgreSQL',
+    image: '/skills/postgresql.svg'
+  }
+]
