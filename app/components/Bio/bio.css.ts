6e10d499c5e130696c17af11e7cfc9fe67acb53d
masatotezuka <tez.0731.mst@gmail.com>
2023-11-17 00:31:03 +0900

    update: bio component

--- app/components/Bio/bio.css.ts
+++ app/components/Bio/bio.css.ts
@@ -1,28 +1,29 @@
 import { style } from '@vanilla-extract/css'
 
-export const container = style({
-  paddingLeft: '50px',
-  borderLeft: '2px solid #333'
+export const timelineContainer = style({
+  borderLeft: '3px solid #333',
+  position: 'relative'
 })
 
-export const eventContainer = style({
-  marginBottom: '20px',
-  padding: '10px 20px',
-  backgroundColor: 'white',
-  boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1)',
-  borderRadius: '4px',
-  width: 'fit-content',
+export const timeline = style({
+  paddingLeft: '50px',
+  paddingBottom: '20px',
   ':before': {
     content: '',
+    width: '12px',
+    height: '12px',
+    background: '#008B8B',
     position: 'absolute',
-    left: '-29px',
-    top: '10px',
-    width: '10px',
-    height: '10px',
-    backgroundColor: '#333',
-    borderRadius: '50%',
-    border: '2px solid white'
+    left: '-7px',
+    top: '0px',
+    borderRadius: '100%'
   }
+})
+
+export const timelineContent = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px'
 })
 
 export const introductionContainer = style({
@@ -32,4 +33,23 @@ export const introductionContainer = style({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center'
+})
+
+export const timelineContentTitle = style({
+  fontSize: '24px',
+  color: '#008B8B',
+  fontWeight: 'bold',
+  margin: '0px'
+})
+
+export const timelineContentDate = style({
+  fontSize: '16px',
+  color: '#008B8B',
+  margin: '0px'
+})
+
+export const timelineContentDescription = style({
+  fontSize: '16px',
+  margin: '10px 0px',
+  lineHeight: '2.0'
 })
--- app/components/Bio/index.tsx
+++ app/components/Bio/index.tsx
@@ -2,12 +2,52 @@ import React, { FC } from 'react'
 import * as styles from './bio.css'
 export const Bio: FC = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.eventContainer}>
-        <p>2023.4 フリーランス</p>
-        <p>2023.3 神戸大学大学院 保健学研究科 卒業</p>
-        <p>2021.3 神戸大学大学院 医学部保健学科 卒業</p>
+    <>
+      <div className={styles.timelineContainer}>
+        <div className={styles.timeline}>
+          <p className={styles.timelineContentTitle}>フリーランス</p>
+          <p className={styles.timelineContentDate}>2023.4〜2023.12</p>
+          <p className={styles.timelineContentDescription}>
+            ・ECコンサル事業のDX支援サービス・人材紹介サービス・製造業のDX推進サービスの新規機能開発{' '}
+            <br />
+            ・担当領域はバックエンド・フロントエンド
+            <br />
+            ・技術スタック: TypeScript / Express / Next.js / MySQL / Docker
+          </p>
+        </div>
       </div>
-    </div>
+      <div className={styles.timelineContainer}>
+        <div className={styles.timeline}>
+          <p className={styles.timelineContentTitle}>神戸大学大学院</p>
+          <p className={styles.timelineContentDate}>2021.4〜2023.3</p>
+          <p className={styles.timelineContentDescription}>
+            ・医療・ヘルスケア分野の国際論文を3本出版 <br />
+            ・研究領域: 産業保健 / 健康経営 / 医療レセプトデータ <br />
+          </p>
+        </div>
+      </div>
+      <div className={styles.timelineContainer}>
+        <div className={styles.timeline}>
+          <p className={styles.timelineContentTitle}>株式会社バックテック(インターン)</p>
+          <p className={styles.timelineContentDate}>2020.3〜2023.3</p>
+          <p className={styles.timelineContentDescription}>
+            ・主にRとBIツール(Redash)使用したデータ分析業務に従事
+            <br />
+            ・ユーザーからの問い合わせ対応をはじめとするカスタマーサポートも経験 <br />
+            ・Node.js(Express), TypeScript(Next.js)によるWebアプリケーションの開発（2023.9〜2023.3）
+          </p>
+        </div>
+      </div>
+      <div className={styles.timelineContainer}>
+        <div className={styles.timeline}>
+          <p className={styles.timelineContentTitle}>神戸大学</p>
+          <p className={styles.timelineContentDate}>2017.4〜2021.3</p>
+          <p className={styles.timelineContentDescription}>
+            ・理学療法学について学び、卒業後に理学療法士の資格を取得
+            <br />
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
