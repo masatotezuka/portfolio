2ffc542f4c77cd3224fdcccd16e67558a4523ab1
masatotezuka <tez.0731.mst@gmail.com>
2023-11-16 22:02:46 +0900

    update about section

--- app/components/About/about.css.ts
+++ app/components/About/about.css.ts
@@ -1,7 +1,12 @@
 import { style } from '@vanilla-extract/css'
 
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '60px'
+})
+
 export const container = style({
-  marginTop: '100px',
   display: 'flex',
   gap: '28px',
   flexGrow: 1
--- app/components/About/index.tsx
+++ app/components/About/index.tsx
@@ -2,31 +2,35 @@ import { FC } from 'react'
 import Image from 'next/image'
 import profileImage from '@/public/profile.png'
 import * as styles from './about.css'
+import { Links } from './Links'
 
 export const About: FC = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <Image
-          src={profileImage}
-          alt='profile image'
-          width={1280}
-          height={852}
-          style={{ width: '100%', height: 'auto' }}
-        />
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src={profileImage}
+            alt='profile image'
+            width={1280}
+            height={852}
+            style={{ width: '100%', height: 'auto' }}
+          />
+        </div>
+        <div className={styles.introductionContainer}>
+          <p>
+            1997年7月31日生まれ <br />
+            長野県安曇野市出身
+            <br />
+            現在は、Webエンジニアをしています。 <br />
+            学生時代は、ヘルスケア分野の研究をしていました。
+            大学院ではヘルスケア分野の国際論文を3本出版し、ヘルスケアスタートアップ企業でプロダクト開発をしています。
+            <br />
+            趣味: 筋トレ・サウナ・アニメ・キャンプ・登山
+          </p>
+        </div>
       </div>
-      <div className={styles.introductionContainer}>
-        <p>
-          1997年7月31日生まれ <br />
-          長野県安曇野市出身
-          <br />
-          現在は、Webエンジニアをしています。 <br />
-          学生時代は、ヘルスケア分野の研究をしていました。
-          大学院ではヘルスケア分野の国際論文を3本出版し、ヘルスケアスタートアップ企業でプロダクト開発をしています。
-          <br />
-          趣味: 筋トレ・サウナ・アニメ・キャンプ・登山
-        </p>
-      </div>
+      <Links />
     </div>
   )
 }
